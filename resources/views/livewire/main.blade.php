18768f2dcc3d57ca7af26c2455666b430d6f2150
Francisco <srfrancisco17@gmail.com>
2024-11-17 20:44:45 -0500

    First commit

---
+++ resources/views/livewire/main.blade.php
@@ -0,0 +1,11 @@
+<div>
+    <section class="flex flex-col items-center space-y-4 py-12">
+        <h1 class="text-3xl font-bold underline">
+            {{$welcome}}
+        </h1>
+        @if (session()->has('message'))
+            <h3 class="bg-blue-400 font-bold p-2 w-1/3 rounded text-center text-sm text-white">{{session('message')}}</h3>
+        @endif
+        <livewire:task />
+    </section>
+</div>
---
+++ resources/views/livewire/task.blade.php
@@ -0,0 +1,34 @@
+<div>
+    <form class="p-4" wire:submit.prevent="save">
+        <div class="mb-4">
+            <input wire:model="task.text" class="p-2 bg-gray-200 w-full" placeholder="Tarea...">
+            @error('task.text')
+                <div class="mt-1 text-red-600 text-sm">{{$message}}</div>
+            @enderror
+        </div>
+        <button type="submit" class="bg-indigo-700 text-white font-bold w-full rounded shadow p-2">Guardar</button>
+    </form>
+    <table class="shadow-md">
+        <thead>
+            <tr class="bg-gray-200 text-gray-600 uppercase text-sm">
+                <th class="py-3 px-6 text-left">Hecha</th>
+                <th class="py-3 px-6 text-left">Tarea</th>
+                <th class="py-3 px-6 text-left">&nbsp;</th>
+            </tr>
+        </thead>
+        <tbody class="text-gray-600">
+            @forelse ($tasks as $task)
+                <tr class="border-b border-gray-200 {{$task->done ? 'bg-green-200' : ''}}">
+                    <td class="px-4 py-2"><input type="checkbox" wire:click="done({{$task->id}})" {{$task->done ? 'checked' : ''}}></td>
+                    <td class="px-4 py-2 {{$task->done ? 'line-through' : ''}}">{{$task->text}}</td>
+                    <td class="px-4 py-2">
+                        <button wire:click="edit({{$task->id}})" type="button" class="bg-indigo-400 px-2 py-1 text-white text-xs rounded">Editar</button>
+                        <button wire:click="delete({{$task->id}})" type="button" class="bg-red-500 px-2 py-1 text-white text-xs rounded">Eliminar</button>
+                    </td>
+                </tr>                
+            @empty
+                <h3>No existen tareas para mostrar.</h3>
+            @endforelse
+        </tbody>
+    </table>
+</div>
